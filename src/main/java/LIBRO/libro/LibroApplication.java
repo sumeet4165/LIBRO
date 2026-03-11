4fe1f7b3df6f07629aa8729a05e76d235d06ec0e
Sumeet Deepak Bhlalerao <sumeet@Sumeets-MacBook-Air.local>
2026-03-11 14:50:34 +0530

    Initial Spring Boot backend project

---
+++ src/main/java/LIBRO/libro/LibroApplication.java
@@ -0,0 +1,13 @@
+package LIBRO.libro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibroApplication.class, args);
+	}
+
+}
